db0c4afa9bdc61bd80e0babcdcd1bf8171026f5c
proje <laamiri.f.fouzia@gmail.com>
2021-06-01 13:55:35 +0200

    ajout dao and service and provided of seance

--- src/main/java/com/example/newprojectoption/service/MyModuleService.java
+++ src/main/java/com/example/newprojectoption/service/MyModuleService.java
@@ -13,6 +13,7 @@ public class MyModuleService {
 
     @Autowired
     private MyModuleDao myModuleDao;
+
     @Autowired
     private ModuleSemestreOptionService moduleSemestreOptionService;
     /****************************************************/
